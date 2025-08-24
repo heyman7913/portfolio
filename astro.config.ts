706cda8b0c81a71031c55929dad24a98079979f6
heyman7913 <chitkarahimansh5@gmail.com>
2025-08-23 22:23:24 -0400

    done

--- astro.config.ts
+++ astro.config.ts
@@ -3,10 +3,16 @@ import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
 import { visualizer } from 'rollup-plugin-visualizer';
-
+import path from 'path';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), compress()],
+  integrations: [
+    tailwind(),
+    image({
+      cacheDir: path.resolve('./.astro/image-cache'),
+    }),
+    compress(),
+  ],
   vite: {
     plugins: [visualizer()],
     resolve: {
